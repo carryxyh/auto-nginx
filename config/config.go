813713062e0059f73c26de09f2b0fd13103ed6a7
ziyuan <ziyuan@2dfire.com>
2017-04-10 20:35:40 +0800

    完善dao

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
-package main
+package config
 
 import (
 	"io/ioutil"
@@ -8,19 +8,19 @@ import (
 )
 
 type DBconfig struct {
-	Listen    string `yaml:"listen"`
-	EndPoints []string `yaml:"endpoints"`
-	BasePath  string   `yaml:"base_path"`
-}
-
-type ETCDconfig struct {
 	Alias      string `yaml:"alias"`
 	DriverName string `yaml:"drive_name"`
 	DataSource string `yaml:"data_source"`
 	Conns      []int `yaml:"conns"`
 }
 
-func getDBconfig() *DBconfig {
+type ETCDconfig struct {
+	Listen    string `yaml:"listen"`
+	EndPoints []string `yaml:"endpoints"`
+	BasePath  string   `yaml:"base_path"`
+}
+
+func GetDBconfig() *DBconfig {
 	filename, _ := filepath.Abs("./config/DBconfig.yml")
 	yamlFile, err := ioutil.ReadFile(filename)
 
@@ -36,7 +36,7 @@ func getDBconfig() *DBconfig {
 	return c
 }
 
-func getETCDconfig() *ETCDconfig {
+func GetETCDconfig() *ETCDconfig {
 	filename, err := filepath.Abs("./config/ETCDconfig.yml")
 	yamlFile, err := ioutil.ReadFile(filename)
 	if err != nil {
--- dao/dao.go
+++ dao/dao.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"auto-ng/config"
 )
 
 type  Template struct {
@@ -11,7 +12,33 @@ type  Template struct {
 	Content     string `orm:"size(200)"`
 }
 
+var db *config.DBconfig
+
 func init() {
+
+	db = config.GetDBconfig()
+
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	//orm.RegisterDataBase()
+
+	orm.RegisterDataBase(db.Alias, db.DriverName, db.DataSource, db.Conns)
+
+	orm.RegisterModel(new(Template))
+
+	orm.RunSyncdb(db.Alias, false, true)
+}
+
+func QueryById() {
+
+}
+
+func Insert() {
+
+}
+
+func DeleteById() {
+
+}
+
+func Update() {
+
 }
