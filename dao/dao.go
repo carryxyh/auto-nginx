cc1f55871e701c2b7db61df472f1bd63c947f38a
ziyuan <ziyuan@2dfire.com>
2017-04-10 20:55:37 +0800

    dao OK

--- dao/dao.go
+++ dao/dao.go
@@ -6,13 +6,15 @@ import (
 	"auto-ng/config"
 )
 
-type  Template struct {
+type Template struct {
 	Id          int
 	ProjectName string `orm:"size(50)"`
 	Content     string `orm:"size(200)"`
 }
 
 var db *config.DBconfig
+
+var ormer orm.Ormer
 
 func init() {
 
@@ -25,20 +27,40 @@ func init() {
 	orm.RegisterModel(new(Template))
 
 	orm.RunSyncdb(db.Alias, false, true)
+
+	ormer = orm.NewOrm()
 }
 
-func QueryById() {
-
+func QueryById(id int) Template {
+	t := Template{Id: id}
+	err := ormer.Read(&t)
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
 
-func Insert() {
-
+func Insert(tmpl *Template) int {
+	id, err := ormer.Insert(&tmpl)
+	if err != nil {
+		panic(err)
+	}
+	return id
 }
 
-func DeleteById() {
-
+func DeleteById(id int) bool {
+	t := Template{Id: id}
+	num, err := ormer.Delete(&t)
+	if err != nil {
+		panic(err)
+	}
+	return num == 1
 }
 
-func Update() {
-
+func Update(tmpl *Template) bool {
+	num, err := ormer.Update(&tmpl)
+	if err != nil {
+		panic(err)
+	}
+	return num == 1
 }
